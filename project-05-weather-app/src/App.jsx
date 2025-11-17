4ef877db64986197cdceb7fa75e1573576304d59
mohamed fayed <memad4858@gmail.com>
2025-11-17 22:00:19 +0200

    add weather starter files

---
+++ project-05-weather-app/src/App.jsx
@@ -0,0 +1,13 @@
+
+const API_URL ="https://api.openweathermap.org/data/2.5/weather?q=paris&appid=49f36df5e17f0cd0c0a96fdcefa40e93&units=metric";
+
+function App() {
+
+  return (
+    <>
+<h1>hello</h1>
+    </>
+  )
+}
+
+export default App
